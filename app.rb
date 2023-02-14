1f867e2a75b130c1cc6991929acbda37cdab97b5
Yussuf Bash <hamzyuss1@gmail.com>
2023-02-14 14:43:32 +0100

    refactor classes

--- app.rb
+++ app.rb
@@ -5,25 +5,9 @@ require_relative 'rental'
 
 class App
   def initialize
-    @cmd = {
-      '1': 'List all books',
-      '2': 'List all people',
-      '3': 'Create a person',
-      '4': 'Create a book',
-      '5': 'Create a rental',
-      '6': 'List all rentals for a given person id',
-      '7': 'Exit'
-    }
-
     @books = []
-
     @people = []
-  end
-
-  def display_cmd
-    @cmd.each do |index, command|
-      puts "#{index} - #{command}"
-    end
+    @rentals = []
   end
 
   def list_all_books
@@ -40,15 +24,9 @@ class App
     if @people.empty?
       puts 'No person added to the system'
     else
-      @people.each do |person|
-        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      @people.each_with_index do |person, index|
+        puts "#{index}. [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
-    end
-  end
-
-  def list_all_people_with_index
-    @people.each_with_index do |person, index|
-      puts "#{index}. [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
   end
 
@@ -108,7 +86,7 @@ class App
     puts 'Book Created Successfully'
   end
 
-  def create_rental()
+  def create_rental
     puts 'Select a book from the list by number'
     list_all_books
     book_option = gets.chomp.to_i
@@ -122,12 +100,12 @@ class App
     print 'Date: '
     date = gets.chomp
 
-    Rental.new(date, @books[book_option], @people[person_option])
+    @rentals.push(Rental.new(date, @books[book_option], @people[person_option]))
 
     puts 'Rental Created Successfully'
   end
 
-  def rentals_of_person()
+  def rentals_of_person
     puts 'All people in the system'
     list_all_people
     print 'Id of the person: '
@@ -146,31 +124,5 @@ class App
         end
       end
     end
-  end
-
-  def choose_option(input)
-    case input
-    when '1' then list_all_books
-    when '2' then list_all_people
-    when '3' then create_person
-    when '4' then create_book
-    when '5' then create_rental
-    when '6' then rentals_of_person
-    end
-  end
-
-  def exit_option
-    input = gets.chomp
-    if input == '7'
-      exit
-    else
-      choose_option(input)
-    end
-  end
-
-  def run
-    puts 'Please choose an option to start'
-    display_cmd
-    exit_option
   end
 end
